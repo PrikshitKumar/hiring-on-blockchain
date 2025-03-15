6e93e594d4bbb3e1d12d619701a558ce5c99b150
Prikshit Kumar <prikshit.cse71@gmail.com>
2025-03-15 23:37:04 +0530

    Added dependencies

--- src/Hiring.sol
+++ src/Hiring.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {HiringStorage} from "./HiringStorage.sol";
 
-contract HiringProcess is Ownable, HiringProcessStorage {
+contract HiringProcess is Initializable, OwnableUpgradeable, HiringStorage {
     event JobPosted(uint256 jobId, string title);
     event ApplicantApplied(uint256 totalApplicants, uint256 jobId, address applicant, bool applied);
     event InterviewRecorded(uint256 jobId, address applicantAddress, uint256 round, string feedback, bool passed);
+    event JobClosed(uint256 jobId);
+
+    function initialize(address _initialOwner) public initializer {
+        __Ownable_init(_initialOwner);
+    }
 
     function postJob(string memory _title, string memory _description) public onlyOwner {
         jobCounter++;
@@ -21,13 +26,13 @@ contract HiringProcess is Ownable, HiringProcessStorage {
         require(!hasApplied[msg.sender][_jobId], "You have already applied for this job");
         
         totalApplicants++;
-        applicants[msg.sender] = Applicant(_jobId, msg.sender, _name, _resumeHash, true);
+        applicants[msg.sender] = Applicant(_jobId, _name, _resumeHash, true);
         hasApplied[msg.sender][_jobId] = true;
         
         emit ApplicantApplied(totalApplicants, _jobId, msg.sender, hasApplied[msg.sender][_jobId]);
     }
 
-    function recordInterview(uint256 _jobId, uint256 _applicantAddress, uint256 _round, string memory _feedback, bool _passed) public onlyOwner {
+    function recordInterview(uint256 _jobId, address _applicantAddress, uint256 _round, string memory _feedback, bool _passed) public onlyOwner {
         require(applicants[_applicantAddress].exists, "Applicant does not exist");
         interviews[_jobId][_applicantAddress].push(Interview(_round, _feedback, _passed));
         emit InterviewRecorded(_jobId, _applicantAddress, _round, _feedback, _passed);
